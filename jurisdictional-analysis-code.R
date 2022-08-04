88a53b1c96dc0049f3b24df2f9d040b983e46111
mtarjan <max.tarjan@gmail.com>
2022-08-04 17:55:54 -0400

    wrangle outputs

--- jurisdictional-analysis-code.R
+++ jurisdictional-analysis-code.R
@@ -28,8 +28,8 @@ reticulate::use_condaenv(condaenv='C:/Program Files/ArcGIS/Pro/bin/Python/envs/a
 
 library(sf)
 library(tidyverse)
-library(exactextractr); library(raster) ##only required for internal r functions
-#library(readxl)
+#library(exactextractr); library(raster) ##only required for internal r functions
+library(readxl)
 
 arcpy<-import("arcpy")
 
@@ -37,22 +37,23 @@ arcpy<-import("arcpy")
 ##layer of spatial extent for selecting species
 #boundary.path<-"G:/cameron/BLM_SMA_overlay/03_SMAgrouped_x_AdminUnits_dsslv.shp"
 ##start with boundary in mobi projection
-boundary.path<-"Data/BLM_National_Surface_Management_Agency/03_SMAgrouped_x_AdminUnits_NAD83.shp"
+boundary.path<-"Data/BLM_National_Surface_Management_Agency/BLM_Lands_NAD83.shp"
 
 ##biotics snapshot
-distribution.data.path <- list.files("H://spp_models/", recursive = TRUE, pattern = "MOBI.tif$", full.names = TRUE)
+distribution.data.path <- list.files("H://spp_models/", recursive = TRUE, pattern = "MOBI(.*?).tif$", full.names = TRUE)
 
 ##SELECT SPECIES FOR OUTPUTS
-library(readxl)
 mobimodels<-read_excel("G:/tarjan/Species-select/Data/MoBI Modeling Summary by Species January 2021.xlsx", sheet = "MoBI_Model_Assessment", skip = 2) %>% data.frame()
 colnames(mobimodels)[1:7]<-c("ELEMENT_GLOBAL_ID", "ELEMENT_GLOBAL_ID2", "cutecode", "Broad Group", "Taxonomic Group", "Scientific Name", "Common Name")
-ja.species <- read_excel("BLMSSS-JA-species-shortlist.xlsx") %>% data.frame()
+ja.species <- read_excel("Data/BLMSSS-JA-species-shortlist.xlsx") %>% data.frame()
 ja.species <- left_join(x = ja.species, y = subset(mobimodels, select = c(cutecode, `Scientific Name`, ELEMENT_GLOBAL_ID)), by = c("NatureServe.Element.ID" = "ELEMENT_GLOBAL_ID"))
 ja.cutecodes<-subset(ja.species, !is.na(cutecode))$cutecode
+##remove cutecodes that have already been completed
+ja.cutecodes<-subset(data.frame(ja.cutecodes), !(ja.cutecodes %in% out$cutecode) & ja.cutecodes != "myotsept")$ja.cutecodes
 
 dir.create("temp_files")
 jur.dat <- dim(0) ##jurisdictional analysis data output
-for (j in 1254:length(distribution.data.path)) { ##for each model; length(distribution.data.path)
+for (j in 1:length(distribution.data.path)) { ##for each model; length(distribution.data.path)
   ##find species
   sp.temp<-str_split(str_split(distribution.data.path[j], "/")[[1]][length(str_split(distribution.data.path[j], "/")[[1]])], pattern="_")[[1]][1]
   
@@ -88,16 +89,36 @@ for (j in 1254:length(distribution.data.path)) { ##for each model; length(distri
 }
 
 ##ALTERNATIVE APPROACH; R PACKAGE
-boundary<-read_sf(boundary.path) ##only needed if doing internal r functions
+#boundary<-read_sf(boundary.path) ##only needed if doing internal r functions
 ##extract sum of raster values and add them up; then convert to area
-mobi.temp<-raster(distribution.data.path[1])
-boundary.nad <- st_transform(x = boundary, st_crs(mobi.temp))
-boundary$sum <- exactextractr::exact_extract(x = mobi.temp, y = boundary.nad, 'sum')
+#mobi.temp<-raster(distribution.data.path[1])
+#boundary.nad <- st_transform(x = boundary, st_crs(mobi.temp))
+#boundary$sum <- exactextractr::exact_extract(x = mobi.temp, y = boundary.nad, 'sum')
 
 ##ALTERNATIVE APPROACH; Tabulate Area
-arcpy$sa$TabulateArea(boundary.path, "BLM_Gen", distribution.data.path[j], "Value", paste0("temp_files/",sp.temp,"_area"), distribution.data.path[j])
+#arcpy$sa$TabulateArea(boundary.path, "BLM_Gen", distribution.data.path[j], "Value", paste0("temp_files/",sp.temp,"_area"), distribution.data.path[j])
 
 
 ##wrangle data into final output
 ##add species name and explorer ID from mobi spreadsheet
 ##sum all BLM_Gen =="BLM" and get a percentage
+##read and rbind outputs from all runs
+outputs <- list.files(getwd(), recursive = F, pattern = ".csv", full.names = TRUE)
+output<-dim(0)
+for (j in 1:length(outputs)) {
+  out.temp<-read.csv(outputs[j])
+  out.temp<-out.temp[,1:5] ##removes column called "j", which is only present for some
+  output<-rbind(output, out.temp)
+}
+
+##summarize results; need a way to add 0s
+output<-unique(output) ##remove duplicates
+out <- subset(output) %>% group_by(cutecode, BLM_Gen) %>% summarise(model.area = total.area, area_m2 = sum(area_m2)) %>% unique() %>% spread(key = BLM_Gen, value = area_m2, fill = 0) %>% data.frame()
+out$percent.model.area.BLM <- round(out$BLM/out$model.area*100, 3)
+head(out)
+
+##output only the species that Bruce needs for preliminary list
+ja.results.shortlist <- left_join(ja.species, subset(out, select = c(cutecode, model.area, BLM, percent.model.area.BLM)))
+write.csv(ja.results.shortlist, "Output/BLMSSS-JA-shortlist-results-20220804.csv", row.names = F)
+
+##add to 
